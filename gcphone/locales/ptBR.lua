5d0e1978490da6683f5457ea404eefc669aeac74
Gabriel F. Vilar <gabriel@cogumm.net>
2021-03-29 11:44:14 -0300

    Novo GCPhone já com alguns ajustes feitos por mim.

---
+++ gcphone/locales/ptBR.lua
@@ -0,0 +1,14 @@
+Locales['pt-BR'] = {
+  ["mission_row"] = "",
+  ["phone_booth"] = "Cabine telefônica",
+  ["key_answer"] = "Pressione ~INPUT_PICKUP~ para atender o telefone",
+  ["new_message"] = "~o~Nova mensagem",
+  ["new_message_from"] = "~o~Nova mensagem de ~y~%s",
+  ["new_message_transmitter"] = "~o~Nova mensagem de ~g~%s",
+  ["use_fixed"] = "~g~%s's phone ~o~(%s) ~n~~INPUT_PICKUP~~w~ Use phone",
+
+	-- Vale
+	["vale_get"] = "Você comprou seu carro por $ 100",
+	["vale_gete"] = "Você pode usar apenas um serviço de manobrista por vez.",
+	["vale_getr"] = "Seu veículo já está marcado no mapa.",
+}
